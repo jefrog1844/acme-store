858181fce6eb6d7fad5ad49ededf1a58e2336ed3
jefrog1844 <jefrog1844@gmail.com>
2021-10-28 14:38:28 -0500

    clean up unit test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -9,81 +9,6 @@ import { ProductsComponent } from './products/products.component';
 import { ProductContainer } from './products/product/product.container';
 import { ProductComponent } from './products/product/product.component';
 import { HttpClientModule } from '@angular/common/http';
-import { CartService } from './cart/cart.service';
-import { ProductsService } from './products/products.service';
-
-class MockCartService {
-  getCart: [];
-  checkout: '';
-}
-class MockProductsService {
-  getProductList: [{
-    'sku': '1',
-    'description': 'Book',
-    'price': 12.49,
-    'countryOfOrigin': 'USA',
-    'exempt': true
-  },
-    {
-      'sku': '2',
-      'description': 'Music CD',
-      'price': 14.99,
-      'countryOfOrigin': 'USA',
-      'exempt': false
-    },
-    {
-      'sku': '3',
-      'description': 'Chocolate Bar',
-      'price': 0.85,
-      'countryOfOrigin': 'USA',
-      'exempt': true
-    },
-    {
-      'sku': '4',
-      'description': 'Imported Box of Chocolates',
-      'price': 10.00,
-      'countryOfOrigin': 'GERMANY',
-      'exempt': true
-    },
-    {
-      'sku': '5',
-      'description': 'Imported Bottle of Perfume',
-      'price': 47.50,
-      'countryOfOrigin': 'ITALY',
-      'exempt': false
-    },
-    {
-      'sku': '6',
-      'description': 'Imported Bottle of Perfume',
-      'price': 27.99,
-      'countryOfOrigin': 'FRANCE',
-      'exempt': false
-    },
-    {
-      'sku': '7',
-      'description': 'Bottle of Perfume',
-      'price': 18.99,
-      'countryOfOrigin': 'USA',
-      'exempt': false
-    },
-    {
-      'sku': '8',
-      'description': 'Packet of Headache Pills',
-      'price': 9.75,
-      'countryOfOrigin': 'USA',
-      'exempt': true
-    },
-    {
-      'sku': '9',
-      'description': 'Imported Box of Chocolates',
-      'price': 11.25,
-      'countryOfOrigin': 'SWEDEN',
-      'exempt': true
-    }];
-  addToCart: null;
-}
-let cartService: CartService;
-let productsService: ProductsService;
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -101,8 +26,6 @@ describe('AppComponent', () => {
         ProductComponent,
       ],
     }).compileComponents();
-    cartService = TestBed.inject(CartService);
-    productsService = TestBed.inject(ProductsService);
   });
 
   it('should create the app', () => {
@@ -111,10 +34,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, () => {
+  it(`should have as title 'ACME Store'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ACME Store');
   });
-
 });
